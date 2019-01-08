e5cdde1d8fe34555d87c2a09879451e4d5d45f6d
Sophia Svane <sophiasvane@Sophias-MacBook-Pro.local>
2019-01-08 14:19:27 +0100

    Al html og css kode validerer

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.addEventListener("load", sidenVises);
 
 function sidenVises() {
     console.log("siden vises");
-    // Herunder vælger vi det, der skal tages fat i.
+    //vi vælger det der skal tages fat i
     document.querySelector("#menu_knap").addEventListener("click", toggleMenu);
 }
 
@@ -10,10 +10,8 @@ function toggleMenu() {
     console.log("toggle menu");
     document.querySelector("#menu_burger").classList.toggle("hidden");
 
-
-    //Her spørger vi om den indeholder klassen "hidden". "let" er den nye måde at lave variabler på.
-    let erSkjult =
-        document.querySelector("#menu_burger").classList.contains("hidden");
+    //vi spørger om den indeholder klassen "hidden". let er den nye måde at lave variabler
+    let erSkjult = document.querySelector("#menu_burger").classList.contains("hidden");
 
     if (erSkjult == true) {
         // menuen er nu skjult - ændre menuknap til tre streger lll
